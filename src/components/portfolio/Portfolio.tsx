bc749fac4dcd70acee86e50506da3b40c5bb83e0
Luis Rosário <l.rosario@campus.fct.unl.pt>
2022-04-14 19:25:22 +0100

    main layout and content of portfolio almost done

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -4,72 +4,84 @@ import React, { useEffect, useState } from 'react';
 import './Portfolio.css';
 import PortfolioItem from './PortolioItem';
 import TableauViz from './TableauViz';
+import { IoLogoTableau } from 'react-icons/io5'
+import { AiFillGithub } from 'react-icons/ai'
+import { SiJupyter } from 'react-icons/si'
+import { MdOutlineWebAsset } from 'react-icons/md'
+import { IconType } from 'react-icons';
 
 
 export type ProjectCardContent = {
   title: string,
   description: String,
   skillImgPath: string,
+  butIco:  string,
   url: string,
-  technologies: string,
+  technologies: string[],
   id: number,
 }
 
 const f1: ProjectCardContent = {
-  title: 'F1 historical overview - Data vizualiazation workbooks',
+  title: 'F1 historical overview - Data vizualiazation',
   description: 'A conjunction of seven data vizualization dashbords intended to answear several research questions regarding historical formula 1 data.',
-  skillImgPath: ''/*require('../../assets/ml.png')*/,
-  technologies: 'Tableau, Phyton.',
-  url: '',
+  skillImgPath: require('../../assets/vizF1.png'),
+  technologies: ['Tableau', 'Phyton'],
+  butIco: 'IoLogoTableau',
+  url: 'https://public.tableau.com/app/profile/luis.ros.rio/viz/F1dataanalysisanhistoricaloverview/Alldash',
   id: 0
 };
 
 
-const javaDev: ProjectCardContent = {
-  title: 'LlemePlay - Angular project',
+const llemmePlay: ProjectCardContent = {
+  title: 'LlemePlay',
   description: 'Web application of a service which facilitates the lending and exchange of hard copy videogames between users.',
-  skillImgPath: ''/*require('../../assets/ml.png')*/,
+  skillImgPath: require('../../assets/lemme.png'),
   url: 'https://trusting-roentgen-21fcd7.netlify.app/',
-  technologies: 'Angular, Typescript.',
+  technologies: ['Angular', 'Typescript'],
+  butIco: 'MdOutlineWebAsset',
   id: 1,
 };
 
-const dataViz: ProjectCardContent = {
-  title: 'Grant approval platfor - Spring + React project',
+const grantMgm: ProjectCardContent = {
+  title: 'Grant approval platform',
   description: 'Grant management system to facilitate the application and attribution of grants to researchers. Frond end developed with react and backend built using the Spring framwork in kotlin.',
-  skillImgPath: require('../../assets/viz.png'),
-  url: '',
-  technologies: 'React, Kotlin, Spring.',
+  skillImgPath: require('../../assets/grant.png'),
+  url: 'https://github.com/Luis-Rosario/grant-app',
+  technologies: ['React', 'Kotlin', 'Spring'],
+  butIco: 'AiFillGithub',
   id: 2,
 };
 
 
-const webDevelopment: ProjectCardContent = {
-  title: 'Web development',
+const mlTutorial: ProjectCardContent = {
+  title: 'Machine learning problems',
   description: 'Familiar with Javascript and Typescript frameworks such as Angular and React to build scalable web applications. Used during my internship and in other academic projects. ',
-  skillImgPath: require('../../assets/webDev.png'),
-  url: '',
-  technologies: 'React, Kotlin, Spring.',
+  skillImgPath: require('../../assets/mlTut.png'),
+  url: 'https://github.com/Luis-Rosario/mlNotebook/blob/main/ml-prob-sol.ipynb',
+  technologies: ['Python', 'Machine', 'Tensorflow', 'keras'],
+  butIco: 'AiFillGithub',
   id: 3,
 };
 
 
-const git: ProjectCardContent = {
-  title: 'Git workflow',
+const licPltCNN: ProjectCardContent = {
+  title: 'CNN for licence plate detection',
   description: 'Experienced with the git workflow. Mostly used Github but also have some experience with Bitbucket.',
-  skillImgPath: require('../../assets/Git.png'),
+  skillImgPath: require('../../assets/licencePlat.png'),
   url: '',
-  technologies: 'React, Kotlin, Spring.',
+  technologies: ['Python', 'Deep learning', 'Tensorflow', 'Convolutional neural networks'],
+  butIco: 'AiFillGithub',
   id: 4
 };
 
 
-const cloud: ProjectCardContent = {
-  title: 'Cloud services',
+const rnnDoor: ProjectCardContent = {
+  title: 'Image text detection with RNN',
   description: 'Have deployed web applications using Azure combining multiple services.',
-  skillImgPath: require('../../assets/azure.png'),
+  skillImgPath: require('../../assets/task3.png'),
   url: '',
-  technologies: 'React, Kotlin, Spring.',
+  technologies: ['Python', 'Deep learning', 'Tensorflow', 'Convolutional neural networks', 'Recurrent neural networks'],
+  butIco: 'AiFillGithub',
   id: 5
 };
 
@@ -86,16 +98,16 @@ function Portfolio() {
 
 
   return (
-    <div id='skills' className="container skills__container">
+    <div id='portfolio' className="container portfolio__container">
       <h2 id='sec-title'>Portfolio</h2>
 
-      <div className='skillItem-container'>
+      <div className='portItem-container'>
         <PortfolioItem content={f1} onSelectItem={handleItemSelect}></PortfolioItem>
-        <PortfolioItem content={javaDev} onSelectItem={handleItemSelect}></PortfolioItem>
-        <PortfolioItem content={dataViz} onSelectItem={handleItemSelect}></PortfolioItem>
-        <PortfolioItem content={webDevelopment} onSelectItem={handleItemSelect}></PortfolioItem>
-        <PortfolioItem content={git} onSelectItem={handleItemSelect}></PortfolioItem>
-        <PortfolioItem content={cloud} onSelectItem={handleItemSelect}></PortfolioItem>
+        <PortfolioItem content={llemmePlay} onSelectItem={handleItemSelect}></PortfolioItem>
+        <PortfolioItem content={grantMgm} onSelectItem={handleItemSelect}></PortfolioItem>
+        <PortfolioItem content={mlTutorial} onSelectItem={handleItemSelect}></PortfolioItem>
+        <PortfolioItem content={licPltCNN} onSelectItem={handleItemSelect}></PortfolioItem>
+        <PortfolioItem content={rnnDoor} onSelectItem={handleItemSelect}></PortfolioItem>
       </div>
       {selectedViz == String(0) && <div className='viz__container'>
         <TableauViz id={selectedViz}></TableauViz>
--- src/components/portfolio/PortolioItem.tsx
+++ src/components/portfolio/PortolioItem.tsx
@@ -4,27 +4,59 @@ import React from 'react';
 import { ProjectCardContent } from './Portfolio';
 
 import '../skills/Skills.css';
+import { IconType } from 'react-icons';
+import { IoLogoTableau } from 'react-icons/io5'
+import { AiFillGithub } from 'react-icons/ai'
+import { SiJupyter } from 'react-icons/si'
+import { MdOutlineWebAsset } from 'react-icons/md'
 
-function PortfolioItem( props: { content: ProjectCardContent, onSelectItem:(item: any) => void}) {
 
-    const handleSelectItem = ( event: React.MouseEvent<HTMLElement>) => {
+
+
+function PortfolioItem(props: { content: ProjectCardContent, onSelectItem: (item: any) => void }) {
+
+    const handleSelectItem = (event: React.MouseEvent<HTMLElement>) => {
         event.preventDefault();
-        let id=(event.target as HTMLElement)!.parentElement!.getAttribute('id')
+        let id = (event.target as HTMLElement)!.parentElement!.getAttribute('id')
         props.onSelectItem(id);
 
-    }
- 
 
+    }
+
+    const keywordItem = props.content.technologies.map((tech: String, index: number) => (
+        <li key={index}> {tech}</li>
+    ))
+
+
+
+
+    const onClickButton  = (event: React.MouseEvent<HTMLElement>) => {
+        event.preventDefault();
+        window.open(props.content.url, '_blank')
+    }
 
     return (
-          <div id={String(props.content.id)} className="skill-card" onClick={(event) => handleSelectItem( event)}>
-            <div className='skill-card-title'>
-                <div className='skill-img'>
-                    <img src={props.content.skillImgPath} />
-                </div>
+        <div className="portItem-card" onClick={(event) => handleSelectItem(event)}>
+            <div className='portItem-card-title'>
+
                 <h3>{props.content.title}</h3>
             </div>
-            <div className='skill-card-content'>
+            <div className='portItem-card-content'>
+                <div id={String(props.content.id)} className='portItem-img'>
+                    <img src={props.content.skillImgPath} />
+                    <div className='keyword-pill'>
+                        <ul className='keyword-pill-list'>
+                            {keywordItem}
+                        </ul>
+                       
+                            {props.content.butIco === 'IoLogoTableau' && <a onClick={(event) => onClickButton(event)} id='icon-port' href={props.content.url} target="_blank"><IoLogoTableau /></a>}
+                            {props.content.butIco === 'AiFillGithub' && <a onClick={(event) => onClickButton(event)} id='icon-port' href={props.content.url} target="_blank"><AiFillGithub /></a>}
+                            {props.content.butIco === 'SiJupyter' && <a onClick={(event) => onClickButton(event)} id='icon-port' href={props.content.url} target="_blank"><SiJupyter /></a>}
+                            {props.content.butIco === 'MdOutlineWebAsset' && <a onClick={(event) => onClickButton(event)} id='icon-port' href={props.content.url} target="_blank"><MdOutlineWebAsset /></a>}
+                        
+                    </div>
+                </div>
+
                 {props.content.description}
             </div>
         </div>
